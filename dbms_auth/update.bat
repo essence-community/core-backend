fca21cc15d8b50f00d3d68dc814d7fd4399bef48
Ivan Artemov <honyrik@gmail.com>
2019-10-01 15:27:51 +0800

    fix update script

--- dbms_auth/update.bat
+++ dbms_auth/update.bat
@@ -1,3 +1,11 @@
 cd %~dp0
-call liquibase\liquibase --defaultsFile=liquibase.meta.properties --changeLogFile=db.changelog.meta.xml update >> main.log
-call liquibase\liquibase --defaultsFile=liquibase.auth.properties --changeLogFile=db.changelog.auth.xml update >> main.log
+set PATHLIQUIBASE=liquibase
+if exist liquibase\liquibase (
+    set PATHLIQUIBASE=
+)
+if exist ..\dbms\liquibase\liquibase (
+    set PATHLIQUIBASE=..\dbms\liquibase\liquibase
+)
+
+call %PATHLIQUIBASE% --defaultsFile=liquibase.meta.properties --changeLogFile=db.changelog.meta.xml update >> main.log
+call %PATHLIQUIBASE% --defaultsFile=liquibase.auth.properties --changeLogFile=db.changelog.auth.xml update >> main.log
