b20b6901b7c295f13c9d272bfde58db9b27911aa
a.romanyuk <a.romanyuk@abr-region.ru>
2019-12-04 15:27:18 +0300

    MTAttrClassList - order by cv_name

--- dbms/t_query/MTAttrClassList.sql
+++ dbms/t_query/MTAttrClassList.sql
@@ -15,6 +15,6 @@ join s_mt.t_class c on
 where
 	ca.ck_attr = (cast(:json as jsonb)->''master''->>''ck_id'')::varchar
 order by
-	ca.ck_attr asc
+	c.cv_name asc
 	 ', 'meta', '20783', '2019-05-27 09:42:20.922637+03', 'select', 'po_session', NULL, 'Необходимо актуализировать')
 on conflict (ck_id) do update set cc_query = excluded.cc_query, ck_provider = excluded.ck_provider, ck_user = excluded.ck_user, ct_change = excluded.ct_change, cr_type = excluded.cr_type, cr_access = excluded.cr_access;
