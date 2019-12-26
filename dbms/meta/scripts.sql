78a233dc0ef7134f0b09604a90fcb7f17c86d626
Ivan Artemov <honyrik@gmail.com>
2019-12-26 20:25:59 +0800

    CORE-650 change meta to static localization

--- dbms/meta/scripts.sql
+++ dbms/meta/scripts.sql
@@ -3,4 +3,5 @@
 select pkg_patcher.p_update_localization();
 --changeset artemov_i:CORE-642 dbms:postgresql runOnChange:true splitStatements:false stripComments:false
 select pkg_patcher.p_delete_dup_localization();
-
+--changeset artemov_i:CORE-650-meta-to-static dbms:postgresql
+select pkg_patcher.p_find_static_in_meta_localization();
