7522008be5153cae4e402665dc0ac67c32cf0160
Ivan Artemov <honyrik@gmail.com>
2020-01-21 21:28:09 +0800

    CORE-797

--- plugins/Patcher/src/auth/RoleAction.ts
+++ plugins/Patcher/src/auth/RoleAction.ts
@@ -2,7 +2,7 @@ import { format } from "util";
 import { IRowPatch } from "../IRowPatch";
 
 const formatRoleActionSqlPostgres =
-    "         select %s as ck_id, (%s)::bigint as ck_action, (%s)::uuid as ck_role, %s as ck_user, %s as ct_change\n";
+    "         select (%s)::uuid as ck_id, (%s)::bigint as ck_action, (%s)::uuid as ck_role, %s as ck_user, %s as ct_change\n";
 
 export class RoleAction extends IRowPatch {
     public toRow(): string {
