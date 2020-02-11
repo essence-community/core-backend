a0f6a64e9da4b3f46c336791e822623392c13cf5
a.romanyuk <a.romanyuk@abr-region.ru>
2020-02-11 15:54:50 +0300

    CORE-935 - add lang_combo + theme_combo to Profile_Page + Patch - Download Button - cv_displayed = Скачать

--- dbms/meta/Page_1875130240361.sql
+++ dbms/meta/Page_1875130240361.sql
@@ -1,3 +1,10 @@
 --liquibase formatted sql
 --changeset patcher-core:Page_1875130240361 dbms:postgresql runOnChange:true splitStatements:false stripComments:false
 INSERT INTO s_mt.t_page (ck_id, ck_parent, cr_type, cv_name, cn_order, cl_static, cv_url, ck_icon, ck_user, ct_change, cl_menu)VALUES('1875130240361', null, 0, '63f4d73447ea4da880234a577c7301b6', 10, 0, null, null, '20780', '2019-03-27T00:00:00.000+0000', 0) on conflict (ck_id) do update set ck_parent = excluded.ck_parent, cr_type = excluded.cr_type, cv_name = excluded.cv_name, cn_order = excluded.cn_order, cl_static = excluded.cl_static, cv_url = excluded.cv_url, ck_icon = excluded.ck_icon, ck_user = excluded.ck_user, ct_change = excluded.ct_change, cl_menu = excluded.cl_menu;
+INSERT INTO s_mt.t_localization (ck_id, ck_d_lang, cr_namespace, cv_value, ck_user, ct_change)
+select t.ck_id, t.ck_d_lang, t.cr_namespace, t.cv_value, t.ck_user, t.ct_change::timestamp from (
+    select '63f4d73447ea4da880234a577c7301b6' as ck_id, 'ru_RU' as ck_d_lang, 'meta' as cr_namespace, 'Меню' as cv_value, '-11' as ck_user, '2019-12-10T00:00:00.000+0000' as ct_change
+) as t 
+ join s_mt.t_d_lang dl
+ on t.ck_d_lang = dl.ck_id
+on conflict on constraint cin_u_localization_1 do update set ck_id = excluded.ck_id, ck_d_lang = excluded.ck_d_lang, cr_namespace = excluded.cr_namespace, cv_value = excluded.cv_value, ck_user = excluded.ck_user, ct_change = excluded.ct_change;
