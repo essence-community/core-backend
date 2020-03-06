51a1d9d5c36990aebb2afb5e45eaee6d4d3a0903
Ivan Artemov <honyrik@gmail.com>
2020-03-06 21:36:21 +0800

    CORE-1040 fix cv_description

--- dbms/t_query/MTGetCurrentDateTime.sql
+++ dbms/t_query/MTGetCurrentDateTime.sql
@@ -3,5 +3,5 @@
 INSERT INTO s_mt.t_query (ck_id,cc_query,ck_provider,ck_user,ct_change,cr_type,cr_access,cv_description)
 VALUES ('MTGetCurrentDateTime','/*MTGetCurrentDateTime*/
 select to_char(current_timestamp, ''IYYY-MM-DD"T"HH24:MI:SS'') as ct_date
-','meta','4fd05ca9-3a9e-4d66-82df-886dfa082113','2020-03-06 09:30:00.000','select','free','Генерация токена')
+','meta','4fd05ca9-3a9e-4d66-82df-886dfa082113','2020-03-06 09:30:00.000','select','free','Текущие время')
 on conflict (ck_id) do update set cc_query = excluded.cc_query, ck_provider = excluded.ck_provider, ck_user = excluded.ck_user, ct_change = excluded.ct_change, cr_type = excluded.cr_type, cr_access = excluded.cr_access, cv_description = excluded.cv_description;
--- dbms_auth/t_query/AuthDefaultTokenExpire.sql
+++ dbms_auth/t_query/AuthDefaultTokenExpire.sql
@@ -3,5 +3,5 @@
 INSERT INTO s_mt.t_query (ck_id,cc_query,ck_provider,ck_user,ct_change,cr_type,cr_access,cv_description)
 VALUES ('AuthDefaultTokenExpire','/*AuthDefaultTokenExpire*/
 select to_char((CURRENT_TIMESTAMP + interval ''2 minute''), ''IYYY-MM-DD"T"HH24:MI:SS'') as ct_date
-','authcore','4fd05ca9-3a9e-4d66-82df-886dfa082113','2020-03-06 09:30:00.000','select','po_session','Генерация токена')
+','authcore','4fd05ca9-3a9e-4d66-82df-886dfa082113','2020-03-06 09:30:00.000','select','po_session','Время истечения токена по умолчанию')
 on conflict (ck_id) do update set cc_query = excluded.cc_query, ck_provider = excluded.ck_provider, ck_user = excluded.ck_user, ct_change = excluded.ct_change, cr_type = excluded.cr_type, cr_access = excluded.cr_access, cv_description = excluded.cv_description;
