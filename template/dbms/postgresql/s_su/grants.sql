7b5c1283da1c0c77221fae4ca5350cc5537f1748
Ivan Artemov <honyrik@gmail.com>
2020-04-15 18:53:11 +0300

    CORE-1149 fix

--- template/dbms/postgresql/s_su/grants.sql
+++ template/dbms/postgresql/s_su/grants.sql
@@ -6,8 +6,6 @@ GRANT USAGE ON SCHEMA pkg_json_user TO ${user.connect};
 
 GRANT USAGE ON SCHEMA pkg_json_patcher TO ${user.connect};
 
-GRANT USAGE ON SCHEMA pkg_json_integr TO ${user.connect};
-
 GRANT USAGE ON SCHEMA ${user.table} TO ${user.connect};
 
 GRANT USAGE ON SCHEMA public TO ${user.update};
@@ -26,14 +24,6 @@ GRANT EXECUTE ON FUNCTION pkg_json_user.f_modify_user_department(pc_json jsonb, 
 
 GRANT EXECUTE ON FUNCTION pkg_json_patcher.f_modify_patch(pv_user character varying, pk_session character varying, pc_json jsonb) TO ${user.connect};
 
-GRANT EXECUTE ON FUNCTION pkg_json_integr.f_modify_d_provider(pv_user character varying, pk_session character varying, pc_json jsonb) TO ${user.connect};
-
-GRANT EXECUTE ON FUNCTION pkg_json_integr.f_modify_d_interface(pv_user character varying, pk_session character varying, pc_json jsonb) TO ${user.connect};
-
-GRANT EXECUTE ON FUNCTION pkg_json_integr.f_modify_interface(pv_user character varying, pk_session character varying, pc_json jsonb) TO ${user.connect};
-
-GRANT EXECUTE ON FUNCTION pkg_json_integr.f_modify_scenario(pv_user character varying, pk_session character varying, pc_json jsonb) TO ${user.connect};
-
 GRANT ALL ON SEQUENCE public.seq_log TO ${user.update};
 
 GRANT SELECT ON TABLE public.dual TO ${user.connect};
@@ -43,22 +33,6 @@ GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE public.dual TO ${user.update};
 GRANT SELECT ON TABLE ${user.table}.t_log TO ${user.connect};
 
 GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE ${user.table}.t_log TO ${user.update};
-
-GRANT SELECT ON TABLE ${user.table}.t_d_interface TO ${user.connect};
-
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE ${user.table}.t_d_interface TO ${user.update};
-
-GRANT SELECT ON TABLE ${user.table}.t_d_provider TO ${user.connect};
-
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE ${user.table}.t_d_provider TO ${user.update};
-
-GRANT SELECT ON TABLE ${user.table}.t_interface TO ${user.connect};
-
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE ${user.table}.t_interface TO ${user.update};
-
-GRANT SELECT ON TABLE ${user.table}.t_scenario TO ${user.connect};
-
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE ${user.table}.t_scenario TO ${user.update};
 
 GRANT SELECT ON TABLE ${user.table}.t_create_patch TO ${user.connect};
 
