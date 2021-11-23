16ef38c01a62edd0f1158145a78dfb70df5f9441
Ivan Artemov <honyrik@gmail.com>
2021-11-23 18:43:23 +0300

    added gradle, remove old

--- template/dbms/oracle/update.bat
+++ template/dbms/oracle/update.bat
@@ -1,11 +1,2 @@
 cd %~dp0
-set PATHLIQUIBASE=liquibase
-if exist liquibase\liquibase (
-    set PATHLIQUIBASE=liquibase\liquibase
-)
-if exist ..\dbms\liquibase\liquibase (
-    set PATHLIQUIBASE=..\dbms\liquibase\liquibase
-)
-
-call %PATHLIQUIBASE% --defaultsFile=liquibase.schema.properties --changeLogFile=db.changelog.schema.xml update >> main.log
-call %PATHLIQUIBASE% --defaultsFile=liquibase.meta.properties --changeLogFile=db.changelog.meta.xml update >> main.log
+call gradlew.bat update -PrunList=schema,meta >> main.log
