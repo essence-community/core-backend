6c1e16e9c23508553b929689cb86f001c21ea50b
Ivan Artemov <honyrik@gmail.com>
2022-04-07 17:15:51 +0300

    Fixed empty

--- dbms/meta/classes_hierarchy/MetaClassHierarchy_7DA104D2491D4C4DA42906EDDF546CBF.sql
+++ dbms/meta/classes_hierarchy/MetaClassHierarchy_7DA104D2491D4C4DA42906EDDF546CBF.sql
@@ -1,8 +1,3 @@
 --liquibase formatted sql
 --changeset patcher-core:MetaClassHierarchy_7DA104D2491D4C4DA42906EDDF546CBF dbms:postgresql runOnChange:true splitStatements:false stripComments:false
-INSERT INTO s_mt.t_class_hierarchy
-(ck_id, ck_class_parent, ck_class_child, ck_class_attr, ck_user, ct_change)
- select t.ck_id, t.ck_class_parent, t.ck_class_child, t.ck_class_attr, t.ck_user, t.ct_change from (
- ) as t
- where t.ck_class_parent in (select ck_id from s_mt.t_class) and t.ck_class_child in (select ck_id from s_mt.t_class)
- on conflict on constraint cin_c_class_hierarchy_1 do update set ck_class_parent = excluded.ck_class_parent, ck_class_child = excluded.ck_class_child, ck_class_attr = excluded.ck_class_attr, ck_user = excluded.ck_user, ct_change = excluded.ct_change;
+
