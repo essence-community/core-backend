36023eb1df9ef23cfd3f2af3433dfc52a8c4554a
Ivan Artemov <honyrik@gmail.com>
2023-03-30 11:04:45 +0300

    Fixed attr page

--- dbms/t_query/MTPageAttr.sql
+++ dbms/t_query/MTPageAttr.sql
@@ -3,6 +3,10 @@
 INSERT INTO s_mt.t_query (ck_id, ck_provider, ck_user, ct_change, cr_type, cr_access, cn_action, cv_description, cc_query)
  VALUES('MTPageAttr', 'meta', '4fd05ca9-3a9e-4d66-82df-886dfa082113', '2020-12-10T11:18:27.503+0000', 'select', 'po_session', null, 'Список атрибутов страницы',
  '/*MTPageAttr*/
+with temp_page as (
+    select * from s_mt.t_page 
+    where ck_id = :json::jsonb#>>''{master,ck_id}''
+)
 select t.* from (
 select 
     pattr.ck_id,
@@ -17,9 +21,13 @@ select
 from
     s_mt.t_attr attr
 left join s_mt.t_page_attr pattr
-    on attr.ck_id = pattr.ck_attr and pattr.ck_page = :json::jsonb#>>''{master,ck_id}''
-where nullif(:json::jsonb#>>''{master,ck_id}'', '''') is not null
-and attr.ck_id in (''defaultvalue'',''activerules'',''redirecturl'',''defaultvaluerule'',''titlerules'')
+    on attr.ck_id = pattr.ck_attr and pattr.ck_page in (select ck_id from temp_page)
+where exists(select 1 from temp_page)
+and (
+    (exists(select 1 from temp_page where cr_type = 2) and attr.ck_id in (''activerules'',''redirecturl'',''titlerules''))
+    or (exists(select 1 from temp_page where cr_type in (0,1)) and attr.ck_id in (''activerules''))
+    or (exists(select 1 from temp_page where cr_type = 3) and attr.ck_id in (''defaultvalue'',''activerules'',''defaultvaluerule''))
+)
 /*##filter.ck_id*/ and pattr.ck_id = (:json::jsonb#>>''{filter,ck_id}'')/*filter.ck_id##*/
  ) as t
 where true
