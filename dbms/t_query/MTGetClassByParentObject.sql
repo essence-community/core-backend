d392e5f0c0ee0a1a777f36955bbd677e2d02e9db
Ivan Artemov <honyrik@gmail.com>
2023-05-14 16:25:46 +0300

    Поправил редактор объектов

--- dbms/t_query/MTGetClassByParentObject.sql
+++ dbms/t_query/MTGetClassByParentObject.sql
@@ -1,6 +1,9 @@
 --liquibase formatted sql
 --changeset artemov_i:MTGetClassByParentObject.sql dbms:postgresql runOnChange:true splitStatements:false stripComments:false
 INSERT INTO s_mt.t_query (ck_id, cc_query, ck_provider, ck_user, ct_change, cr_type, cr_access, cn_action, cv_description) VALUES ('MTGetClassByParentObject', '/*MTGetClassByParentObject*/
+with tt_master as (
+  select nullif(trim(:json::jsonb#>>''{master,ck_id}''),'''') as ck_id
+)
 select ck_id,
        cv_name,
        cv_description,
@@ -13,18 +16,21 @@ select ck_id,
   from (select distinct 
                c.*
           from s_mt.t_class c
-          left join s_mt.t_class_hierarchy ch on ch.ck_class_child  = c.ck_id
-          left join s_mt.t_object          o  on ch.ck_class_parent = o.ck_class
-                                              or c.ck_id = o.ck_class
          where 1 = 1
            /* только те классы, которые могут быть дочерними от класса родительского объекта */
            /*##master.ck_id*/
-           and o.ck_id = (cast(:json as jsonb)->''master''->>''ck_id'')::varchar/*master.ck_id##*/
+           and c.ck_id in (
+                select ch.ck_class_child from s_mt.t_class_hierarchy ch
+                join s_mt.t_object o
+                on o.ck_class = ch.ck_class_parent
+                where o.ck_id in (select ck_id from tt_master)
+            )
+           /*master.ck_id##*/
            and (c.ck_view = :json::json#>>''{filter,ck_view}'' or c.ck_view = ''system'')
            /* если родительский объект не передан, то показываем только final-классы */
-           and ((cast(:json as jsonb)->''master''->>''ck_id'')::varchar is not null or c.cl_final = 1)
+           and (not exists(select 1 from tt_master where ck_id is null) or c.cl_final = 1)
   ) as q
  order by cv_displayed
-  ', 'meta', '20783', '2019-05-27 14:23:16.322778+03', 'select', 'po_session', NULL, 'Необходимо актуализировать')
+  ', 'meta', '20783', '2019-05-27 14:23:16.322778+03', 'select', 'po_session', NULL, 'Список дочерних классов')
 on conflict (ck_id) do update set cc_query = excluded.cc_query, ck_provider = excluded.ck_provider, ck_user = excluded.ck_user, ct_change = excluded.ct_change, cr_type = excluded.cr_type, cr_access = excluded.cr_access, cn_action = excluded.cn_action, cv_description = excluded.cv_description;
 
