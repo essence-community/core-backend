db30114900227e06b6302b7ce7429fbc6c00adef
Ivan Artemov <honyrik@gmail.com>
2023-06-05 14:02:35 +0300

    fixed regexp

--- dbms/s_mp/pkg_util.sql
+++ dbms/s_mp/pkg_util.sql
@@ -39,16 +39,16 @@ CREATE FUNCTION pkg_util.f_get_global_from_string(pv_string character varying DE
     AS $$declare
   res ct_varchar;
 declare
-   vv_pattern VARCHAR := '(?!\w|\").([g][A-z_0-9]+)|^([g][A-z_0-9]+)';
+   vv_pattern VARCHAR := '(?!\w|\").([g]\w+)|^([g]\w+)';
 begin
   res := '{}'::ct_varchar;
   if pv_string is not null and pv_string !~ '^[\[\{]' then
     if pv_attr = 'setglobal' or pv_attr = 'setrecordtoglobal' then
       -- ищем глобальные переменные в правой части равенства или в начале строки
-      vv_pattern := '\=([g][A-z_0-9]+)|^([g][A-z_0-9]+)$';
+      vv_pattern := '\=([g]\w+)|^([g]\w+)$';
     elsif pv_attr = 'columnsfilter' then
       -- ищем все глобальные переменные, за исключением правой части равенства
-      vv_pattern := '(?!\w|\"|\=).([g][A-z_0-9]+)|^([g][A-z_0-9]+)';
+      vv_pattern := '(?!\w|\"|\=).([g]\w+)|^([g]\w+)';
     end if;
     begin
       select array_agg(coalesce(t.res[1], t.res[2])) as global
