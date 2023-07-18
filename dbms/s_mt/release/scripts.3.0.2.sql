5884c63fb09ef9b6312d24f87b2b45b5042360aa
Ivan Artemov <honyrik@gmail.com>
2023-07-18 10:37:08 +0300

    fixed type refresh page

---
+++ dbms/s_mt/release/scripts.3.0.2.sql
@@ -0,0 +1,11 @@
+--liquibase formatted sql
+--changeset artemov_i:fixed_type_ot_save_poa dbms:postgresql splitStatements:false stripComments:false
+DROP TYPE public.ot_save_poa;
+
+CREATE TYPE public.ot_save_poa as
+(cv_path varchar, /* путь к объекту */
+ ck_class_attr varchar(32), /* ИД атрибута класса (из POA) */
+ cv_value varchar, /* Значение атрибута (из POA) */
+ ck_user varchar(150),
+ ct_change timestamptz
+);
