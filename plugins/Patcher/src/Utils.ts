0d8eb842e95108128e5a50b5b974b3a396771d3f
Ivan Artemov <honyrik@gmail.com>
2025-11-27 10:25:42 +0300

    Fixed patcher

--- plugins/Patcher/src/Utils.ts
+++ plugins/Patcher/src/Utils.ts
@@ -5,7 +5,7 @@ export function createWriteStream(dir: string, name: string) {
     const stream = fs.createWriteStream(path.join(dir, name + ".sql"));
     stream.write("--liquibase formatted sql\n");
     stream.write(
-        `--changeset patcher-core:${name} dbms:postgresql runOnChange:true splitStatements:false stripComments:false\n`,
+        `--changeset patcher-core:${name}_${new Date().getTime()} dbms:postgresql runOnChange:true splitStatements:false stripComments:false\n`,
     );
     return stream;
 }
@@ -27,13 +27,13 @@ export function createChangeXml(pathFile: string, include: string[]) {
         fs.writeFile(
             pathFile,
             '<?xml version="1.0" encoding="UTF-8"?>\n' +
-                "<databaseChangeLog\n" +
-                '  xmlns="http://www.liquibase.org/xml/ns/dbchangelog"\n' +
-                '  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
-                '  xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog\n' +
-                '         http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.1.xsd">\n' +
-                include.join("") +
-                "\n</databaseChangeLog>",
+            "<databaseChangeLog\n" +
+            '  xmlns="http://www.liquibase.org/xml/ns/dbchangelog"\n' +
+            '  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
+            '  xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog\n' +
+            '         http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.1.xsd">\n' +
+            include.join("") +
+            "\n</databaseChangeLog>",
             (err) => {
                 if (err) {
                     return reject(err);
